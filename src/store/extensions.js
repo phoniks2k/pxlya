9a47b90e04d2b9fde998387a498cc9033f8b546d
HF <pixelplanetdev@gmail.com>
2020-11-19 22:20:08 +0100

    add comments and set_hover to extensions eventemitter

--- src/store/extensions.js
+++ src/store/extensions.js
@@ -12,34 +12,48 @@ const pixelPlanetEvents = new EventEmitter();
 export default () => (next) => (action) => {
   switch (action.type) {
     case 'SELECT_CANVAS': {
-      const { canvasId } = action;
-      pixelPlanetEvents.emit('selectcanvas', canvasId);
+      pixelPlanetEvents.emit('selectcanvas', action.canvasId);
       break;
     }
 
     case 'SET_VIEW_COORDINATES': {
-      const { view } = action;
-      pixelPlanetEvents.emit('setviewcoordinates', view);
+      /*
+       * view: [x, y] canvas coordinates of the center of the screen
+       */
+      pixelPlanetEvents.emit('setviewcoordinates', action.view);
+      break;
+    }
+
+    case 'SET_HOVER': {
+      /*
+       * hover: [x, y] canvas coordinates of cursor
+       * just used on 2D canvas
+       */
+      pixelPlanetEvents.emit('sethover', action.hover);
       break;
     }
 
     case 'RECEIVE_BIG_CHUNK': {
+      /*
+       * center: Array with [zoom, x, y] on 2D and [0, x, z] on 3D canvas
+       * chunk: ChunkRGB or ChunkRGB3D object,
+       *        see ui/ChunkRGB.js and ui/ChunkRGB3D.js
+       */
       const { center, chunk } = action;
       pixelPlanetEvents.emit('receivechunk', center, chunk);
       break;
     }
 
     case 'RECEIVE_PIXEL_UPDATE': {
+      /*
+       * i, j: chunk coordinates
+       * offset: position of pixel within chunk
+       * color: color index
+       */
       const {
         i, j, offset, color,
       } = action;
       pixelPlanetEvents.emit('pixelupdate', i, j, offset, color);
-      break;
-    }
-
-    case 'RECEIVE_ME': {
-      const { canvases } = action;
-      pixelPlanetEvents.emit('getcanvases', canvases);
       break;
     }
 
