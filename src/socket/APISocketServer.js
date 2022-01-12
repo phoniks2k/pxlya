4753acfae2e7dba528a598c67e1a6fa1dacfd1b4
HF <pixelplanetdev@gmail.com>
2022-01-12 01:50:51 +0100

    fix APISocket

--- src/socket/APISocketServer.js
+++ src/socket/APISocketServer.js
@@ -62,8 +62,9 @@ class APISocketServer {
       ws.subPxl = false;
       ws.on('pong', heartbeat);
 
-      ws.on('message', (message) => {
-        if (typeof message === 'string') {
+      ws.on('message', (data, isBinary) => {
+        if (!isBinary) {
+          const message = data.toString();
           this.onTextMessage(message, ws);
         }
       });
