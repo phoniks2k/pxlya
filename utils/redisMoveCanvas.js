e0ce282676731bce304e0e066e9536b3e481b7fc
HF <hf@pixelplanet.fun>
2022-06-28 00:36:19 +0200

    update redisMoveCanvas.js util script

--- utils/redisMoveCanvas.js
+++ utils/redisMoveCanvas.js
@@ -1,25 +1,25 @@
 // this script moves chunks of a canvas, i.e. to center it after changing size
 
-import redis from 'redis';
+import { createClient } from 'redis';
 
 //ATTENTION Make suer to set the rdis URLs right!!!
 const url = "redis://localhost:6379";
-const redisc = redis.createClient({ url, return_buffers: true });
+const redisc = createClient({ url });
 
-const CANVAS_SIZE = 4096;
+const CANVAS_SIZE = 1024;
 const TILE_SIZE = 256;
-const offset = (16384 - 4096) / 2 / 256;
+const offset = (2048 - 1024) / 2 / 256;
 
 const CHUNKS_XY = CANVAS_SIZE / TILE_SIZE;
 
 async function move() {
   for (let x = CHUNKS_XY - 1; x >= 0; x--) {
     for (let y = CHUNKS_XY - 1; y >= 0; y--) {
-      const key = `ch:1:${x}:${y}`;
-      const chunk = await redisc.get(key);
+      const key = `ch:8:${x}:${y}`;
+      const chunk = await redisc.get(key, { returnBuffers: true });
       if (chunk) {
         const buffer = new Uint8Array(chunk);
-        const newKey = `ch:1:${x + offset}:${y + offset}`
+        const newKey = `ch:8:${x + offset}:${y + offset}`
         await redisc.set(newKey, Buffer.from(buffer.buffer));
         await redisc.del(key);
         console.log('Moved Chunk ', key, ' to ', newKey);
