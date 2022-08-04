53b8168f24de13a44a2f6110f3dd26339b090026
HF <hf@pixelplanet.fun>
2022-08-05 01:32:08 +0200

    add Ban sql table and remove blacklist

---
+++ src/data/sql/Ban.js
@@ -0,0 +1,36 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './sequelize';
+
+const Ban = sequelize.define('Blacklist', {
+  ip: {
+    type: DataTypes.CHAR(39),
+    allowNull: false,
+    primaryKey: true,
+  },
+
+  reason: {
+    type: `${DataTypes.CHAR(200)} CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci`,
+    allowNull: false,
+  },
+
+  /*
+   * wpiration time,
+   * NULL if infinite
+   */
+  expires: {
+    type: DataTypes.DATE,
+  },
+
+  /*
+   * uid of mod who made the ban
+   */
+  muid: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+  updatedAt: false,
+});
+
+export default Ban;
