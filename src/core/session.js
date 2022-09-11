fc602934dc141bb9e9f2091034e1fd7bc3e34b83
HF <hf@pixelplanet.fun>
2022-09-11 05:03:20 +0200

    change sessions

--- src/core/session.js
+++ src/core/session.js
@@ -1,7 +1,7 @@
 /*
  *
  */
-import expressSession from 'express-session';
+import session from 'express-session';
 import RedisStore from '../utils/connectRedis';
 
 import client from '../data/redis/client';
@@ -12,31 +12,20 @@ import { SESSION_SECRET } from './config';
 
 export const store = new RedisStore({ client });
 
-/*
- * we cache created session middlewares per domain
- */
-const middlewareCache = {};
-
 export default (req, res, next) => {
   const domain = getHostFromRequest(req, false, true);
-  console.log('THISHTISTHIST', domain);
-  console.log(req.headers);
-  let session = middlewareCache[domain];
-  if (!session) {
-    session = expressSession({
-      name: COOKIE_SESSION_NAME,
-      store,
-      secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        domain,
-        httpOnly: true,
-        secure: false,
-        maxAge: 30 * 24 * HOUR,
-      },
-    });
-    middlewareCache[domain] = session;
-  }
-  return session(req, res, next);
+  const sess = session({
+    name: COOKIE_SESSION_NAME,
+    store,
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      domain,
+      httpOnly: true,
+      secure: false,
+      maxAge: 30 * 24 * HOUR,
+    },
+  });
+  return sess(req, res, next);
 };
