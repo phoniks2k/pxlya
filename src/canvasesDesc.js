7013601f1fd6642681e939b3332f174b795eecae
sallbet <sallbet@mail.ru>
2023-08-02 15:50:24 +0300

    Add new canvas

--- src/canvasesDesc.js
+++ src/canvasesDesc.js
@@ -24,6 +24,7 @@ function getCanvases(t) {
     6: t`PixelCanvas`,
     7: t`1bit`,
     8: t`Top10`,
+    9: t`r/place`,
   };
   const canvasDesc = {
     0: t`Our main canvas, a huge map of the world. Place everywhere you like`,
@@ -34,6 +35,7 @@ function getCanvases(t) {
     6: t`Mirror of PixelCanvas`,
     7: t`Black and White canvas`,
     8: t`A canvas for the most active players from the the previous day. Daily ranking updates at 00:00 UTC.`,
+    9: t`Copy of r/place 2023 canvas. Will be present for a while.`,
   };
   /*
    * no edit below here needed when adding/removing canvas
